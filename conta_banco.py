a283f77cfff0e8c8fa51515a17a99139c29cc424
DanielCastelo <53979258+DanielCastelo@users.noreply.github.com>
2019-08-10 15:24:27 -0300

    Orient-Obj
    Exclusivo para o Python Console.

---
+++ conta_banco.py
@@ -0,0 +1,56 @@
+#Código feito para uso no Python Console
+
+class Conta:
+
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto {}".format(self))
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print("O saldo do titular {} é R$ {}".format(self.__titular, self.__saldo))
+
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def __pode_sacar(self, valor_a_sacar):
+        valor_dispinivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_dispinivel_a_sacar
+
+    def saca(self, valor):
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite".format(valor))
+
+    def transferir(self, valor, origem, destino):
+        origem.saca(valor)
+        destino.deposita(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def set_limite(self, limite):
+        self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco': '237'}
